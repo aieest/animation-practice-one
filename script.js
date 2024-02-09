ce95da47d109f6aae40c91cfab7c7306dfded72f
aieest <aie.estidola@gmail.com>
2024-02-09 12:41:06 +0800

    Combined both functions

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 const button = document.querySelector('#button');
 const div = document.querySelector('#main-div')
 
-function changeButtonClass() {
+function changeClass() {
     if (button.classList.contains("activate")) {
         button.classList.remove("activate");
         button.classList.add("return");
         button.textContent = "Return!";
+        div.classList.remove("dots-inactive")
+        div.classList.add("dots-active")
     } else if (button.classList.contains("return")) {
         button.classList.remove("return");
         button.classList.add("activate");
         button.textContent = "Click Me!";
-    }
-}
-
-function changeDivClass() {
-    if (button.classList.contains("activate")) {
-        div.classList.remove("dots-inactive")
-        div.classList.add("dots-active")
-    } else if (button.classList.contains("dots-inactive")) {
         div.classList.remove("dots-active")
         div.classList.add("dots-inactive")
     }
-}
-
-function changeBackground() {
-    changeButtonClass();
-    changeDivClass();
 }
