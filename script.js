608baff8f2c58e7b48437a59b73b5724e53a61f0
aieest <aie.estidola@gmail.com>
2024-02-09 12:21:33 +0800

    Created changeDivClass Function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const button = document.querySelector('#button');
+const div = document.querySelector('')
 
 function changeButtonClass() {
     const activateButton = document.querySelector('.activate');
@@ -12,5 +13,15 @@ function changeButtonClass() {
         button.classList.remove("return");
         button.classList.add("activate");
         button.textContent = "Click Me!";
+    }
+}
+
+function changeDivClass() {
+    if (button.classList.contains("activate")) {
+        div.classList.remove("dots-inactive")
+        div.classList.add("dots-active")
+    } else if (button.classList.contains("dots-inactive")) {
+        div.classList.remove("dots-active")
+        div.classList.add("dots-inactive")
     }
 }
