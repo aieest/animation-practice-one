8b7d0e85c5f5ca2b6168bedc337db9043d4bd172
aieest <aie.estidola@gmail.com>
2024-02-16 12:05:21 +0800

    Created span mulitplier

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const button = document.querySelector('#button');
 const span = document.querySelector('.span');
 const spanDiv = document.querySelector('.span-div');
-const numberOfSpans = Math.floor((window.screen.width * window.screen.height) / 100);
+const numberOfSpans = Math.floor((window.screen.width * window.screen.height) / 5250);
+
 
 function changeClass() {
     if (button.classList.contains("activate")) {
@@ -17,4 +18,10 @@ function changeClass() {
         span.classList.remove("dots-active")
         span.classList.add("dots-inactive")
     }
+}
+
+for (let i = 1; i <= numberOfSpans; i++) {
+    const newSpan = document.createElement('span');
+    newSpan.classList.add('span', 'dots-inactive');
+    spanDiv.appendChild(newSpan);
 }
